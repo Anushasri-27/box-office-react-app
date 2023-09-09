893caccc20edcc6d7743981568347926da0cb388
Anushasri-27 <anushasrivastava1999@gmail.com>
2023-09-09 20:45:01 +0530

    isolated search form logic into another component

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+
+
+const SearchForm = ({onSearch} ) =>{
+    
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [searchStr, setSearchStr] = useState(' ');
+  const [searchOption, setSearchOption] = useState('shows'); // can be shows aor actor
+
+  
+  const onInputChange = ev => {
+        setSearchStr(ev.target.value);
+      };
+    
+  const onRadioChange = ev =>{
+          
+        setSearchOption(ev.target.value);
+       
+      };
+  const onSubmit = ev => {
+    ev.preventDefault();
+    const options = {
+      q: searchStr,
+      searchOption
+    }
+    onSearch(options);
+  }   
+
+    return(
+
+        <form name="search" id="search-box" onSubmit={onSubmit}>
+        <input
+          name="blog"
+          value={searchStr}                        //adding value attribute to implemt two wau data binding
+          type="text"
+          onChange={onInputChange}
+        />
+        <br></br>
+        <label>
+          shows
+          <input
+            type="radio"
+            name="search-option"
+            checked={searchOption === 'shows'}
+            onChange={onRadioChange}
+            value="shows"
+          />
+        </label>
+        <br></br>
+        <label>
+          actors
+          <input
+            type="radio"
+            name="search-option"
+            checked={searchOption === 'actors'}
+            onChange={onRadioChange}
+            value="actors"
+          />
+        </label>
+        <button type="submit" className="btn-style">
+          Search
+        </button>
+      </form>
+    )
+}
+
+export default SearchForm;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,32 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react';
 import { searchForShow , searchForPeople } from '../api/tvmaze';
+import SerachForm from '../components/SearchForm';
 const Home = () => {
-  const [searchStr, setSearchStr] = useState(' ');
+ 
   const [apiData, setApiData] = useState(null);             //null-->array[]
   const [apiDataError, setApiDataError] = useState(null);
-  const [searchOption, setSearchOption] = useState('shows'); // can be shows aor actor
 
-  const onInputChange = ev => {
-    setSearchStr(ev.target.value);
-  };
 
-  const onRadioChange = ev =>{
-      
-    setSearchOption(ev.target.value);
-    console.log(searchOption);
-  };
 
   
-  const onSearch = async ev => {        //method trigger when submit is clicked
+  const onSearch = async ({q, searchOption})=> {        //method trigger when submit is clicked
     
-    ev.preventDefault();                 //prevent default behavior of submit
-    
+  //prevent default behavior of submit
+   
     try {                                 //handling error
       setApiDataError(null);
-
+      let result;
       if(searchOption === 'shows'){
   
-        const result = await searchForShow(searchStr);
+         result = await searchForShow(q);
         setApiData(result);
       }else{
-        const result = await searchForPeople(searchStr);
-        setApiData(result);
-      } 
+         result = await searchForPeople(q);
+       
+      }
+      
+      setApiData(result);
     } catch (error) {
       setApiDataError(error);
     }
@@ -57,39 +51,7 @@ const Home = () => {
   return (
     <>
       <div className="search-container">
-        <form name="search" id="search-box" onSubmit={onSearch}>
-          <input
-            name="blog"
-            value={searchStr}                        //adding value attribute to implemt two wau data binding
-            type="text"
-            onChange={onInputChange}
-          />
-          <br></br>
-          <label>
-            shows
-            <input
-              type="radio"
-              name="search-option"
-              checked={searchOption === 'shows'}
-              onChange={onRadioChange}
-              value="shows"
-            />
-          </label>
-          <br></br>
-          <label>
-            actors
-            <input
-              type="radio"
-              name="search-option"
-              checked={searchOption === 'actors'}
-              onChange={onRadioChange}
-              value="actors"
-            />
-          </label>
-          <button type="submit" className="btn-style">
-            Search
-          </button>
-        </form>
+         <SerachForm onSearch={onSearch} />
 
         <div>{renderApiData()}</div>
       </div>
